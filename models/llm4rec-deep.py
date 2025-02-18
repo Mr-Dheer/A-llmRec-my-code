b16ddd78ee68d40a92f58035d3cf8b5f2d501fe2
Mr-Dheer <22kavachdheer@gmail.com>
2025-02-18 16:39:56 +0000

    deepseek model

---
+++ models/llm4rec-deep.py
@@ -0,0 +1,148 @@
+import torch
+import torch.nn as nn
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import BitsAndBytesConfig
+
+
+class llm4rec(nn.Module):
+    def __init__(
+            self,
+            device,
+            llm_model="",
+            max_output_txt_len=256,
+    ):
+        super().__init__()
+        self.device = device
+
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_use_double_quant=True
+        )
+
+        if llm_model == 'deepseek_r1':
+            self.llm_model = AutoModelForCausalLM.from_pretrained(
+                "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+                quantization_config=quantization_config,
+                device_map="auto",
+                torch_dtype=torch.float16
+            )
+            self.llm_tokenizer = AutoTokenizer.from_pretrained(
+                "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+                use_fast=True
+            )
+        else:
+            raise Exception(f'{llm_model} is not supported. Please specify "deepseek_r1".')
+
+        self.llm_tokenizer.add_special_tokens({
+            'additional_special_tokens': ['[PAD]', '<s>', '</s>', '[UserRep]', '[HistoryEmb]', '[CandidateEmb]']
+        })
+        self.llm_tokenizer.pad_token = '[PAD]'
+        self.llm_tokenizer.bos_token = '<s>'
+        self.llm_tokenizer.eos_token = '</s>'
+        self.llm_tokenizer.unk_token = '</s>'
+
+        self.llm_model.resize_token_embeddings(len(self.llm_tokenizer))
+
+        for _, param in self.llm_model.named_parameters():
+            param.requires_grad = False
+
+        self.max_output_txt_len = max_output_txt_len
+
+    def concat_text_input_output(self, input_ids, input_atts, output_ids, output_atts):
+        input_part_targets_len = []
+        llm_tokens = {"input_ids": [], "attention_mask": []}
+        for i in range(input_ids.size(0)):
+            this_input_ones = input_atts[i].sum()
+            input_part_targets_len.append(this_input_ones)
+            llm_tokens['input_ids'].append(
+                torch.cat([
+                    input_ids[i][:this_input_ones],
+                    output_ids[i][1:],
+                    input_ids[i][this_input_ones:]
+                ])
+            )
+            llm_tokens['attention_mask'].append(
+                torch.cat([
+                    input_atts[i][:this_input_ones],
+                    output_atts[i][1:],
+                    input_atts[i][this_input_ones:]
+                ])
+            )
+        llm_tokens['input_ids'] = torch.stack(llm_tokens['input_ids'])
+        llm_tokens['attention_mask'] = torch.stack(llm_tokens['attention_mask'])
+        return llm_tokens, input_part_targets_len
+
+    def replace_hist_candi_token(self, llm_tokens, inputs_embeds, interact_embs, candidate_embs):
+        if len(interact_embs) == 0:
+            return llm_tokens, inputs_embeds
+
+        history_token_id = self.llm_tokenizer("[HistoryEmb]", return_tensors="pt",
+                                              add_special_tokens=False).input_ids.item()
+        candidate_token_id = self.llm_tokenizer("[CandidateEmb]", return_tensors="pt",
+                                                add_special_tokens=False).input_ids.item()
+
+        for inx in range(len(llm_tokens["input_ids"])):
+            idx_tensor = (llm_tokens["input_ids"][inx] == history_token_id).nonzero().view(-1)
+            for idx, item_emb in zip(idx_tensor, interact_embs[inx]):
+                inputs_embeds[inx][idx] = item_emb
+
+            idx_tensor = (llm_tokens["input_ids"][inx] == candidate_token_id).nonzero().view(-1)
+            for idx, item_emb in zip(idx_tensor, candidate_embs[inx]):
+                inputs_embeds[inx][idx] = item_emb
+
+        return llm_tokens, inputs_embeds
+
+    def forward(self, log_emb, samples):
+        atts_llm = torch.ones(log_emb.size()[:-1], dtype=torch.long).to(self.device)
+        atts_llm = atts_llm.unsqueeze(1)
+
+        text_output_tokens = self.llm_tokenizer(
+            [t + self.llm_tokenizer.eos_token for t in samples['text_output']],
+            return_tensors="pt",
+            padding="longest",
+            truncation=False,
+        ).to(self.device)
+
+        text_input_tokens = self.llm_tokenizer(
+            samples['text_input'],
+            return_tensors="pt",
+            padding="longest",
+            truncation=False,
+        ).to(self.device)
+
+        llm_tokens, input_part_targets_len = self.concat_text_input_output(
+            text_input_tokens.input_ids,
+            text_input_tokens.attention_mask,
+            text_output_tokens.input_ids,
+            text_output_tokens.attention_mask,
+        )
+
+        targets = llm_tokens['input_ids'].masked_fill(llm_tokens['input_ids'] == self.llm_tokenizer.pad_token_id, -100)
+
+        for i, l in enumerate(input_part_targets_len):
+            targets[i][:l] = -100
+
+        empty_targets = torch.ones(atts_llm.size(), dtype=torch.long).to(self.device).fill_(-100)
+
+        targets = torch.cat([empty_targets, targets], dim=1)
+
+        inputs_embeds = self.llm_model.get_input_embeddings()(llm_tokens['input_ids'])
+        llm_tokens, inputs_embeds = self.replace_hist_candi_token(llm_tokens, inputs_embeds, samples['interact'],
+                                                                  samples['candidate'])
+        attention_mask = llm_tokens['attention_mask']
+
+        log_emb = log_emb.unsqueeze(1)
+        inputs_embeds = torch.cat([log_emb, inputs_embeds], dim=1)
+        attention_mask = torch.cat([atts_llm, llm_tokens['attention_mask']], dim=1)
+
+        with torch.amp.autocast(device_type='cuda', dtype=torch.float16):
+            outputs = self.llm_model(
+                inputs_embeds=inputs_embeds,
+                attention_mask=attention_mask,
+                labels=targets
+            )
+        loss = outputs.loss
+
+        return loss
